05d348b6df29c60c447b3e0f4669b8331b70183c
HannerB <hannerb48@gmail.com>
2024-07-08 17:25:39 -0500

    AGG - Actualización en tablas y .env.

--- database/migrations/2024_07_08_215834_create_calificaciones_table.php
+++ database/migrations/2024_07_08_215834_create_calificaciones_table.php
@@ -12,8 +12,15 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('calificaciones', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+            $table->bigIncrements('id_calificacion');
+            $table->bigInteger('idpane')->nullable();
+            $table->bigInteger('producto')->nullable(false);
+            $table->tinyInteger('prueba')->nullable(false)->comment('1=TRIANGULAR,2=DUO-TRIO,3=ORDENAMIENTO');
+            $table->string('atributo', 50)->nullable(false);
+            $table->string('cod_muestras', 250)->nullable(false);
+            $table->string('comentario', 250)->nullable();
+            $table->date('fecha')->nullable();
+            $table->integer('cabina')->nullable(false);
         });
     }
 
